65a4ea91d49e72b42c5ead54ce8e98d9b1c7983e
Reme Le Hen <reme.lehane@gmail.com>
2021-08-08 16:47:41 +0200

    feat(app): [] setting up gin request handler

--- api/infrastructure/endpoints/endpoints.go
+++ api/infrastructure/endpoints/endpoints.go
@@ -1,8 +1,12 @@
 package endpoints
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/RemeJuan/lattr/business/template"
 	"github.com/RemeJuan/lattr/infrastructure/web-hooks"
 	"github.com/gin-gonic/gin"
 )
@@ -11,14 +15,35 @@ func Register() {
 	r := gin.Default()
 	g := r.Group("/")
 	{
-		g.GET("/templates/:id", templates)
+		g.GET("/templates/:id", handleTemplates)
+		g.POST("/templates", handleTemplates)
 		g.POST("/webhook", web_hooks.HandleWebhook)
 	}
 	log.Fatalln(r.Run(":8080"))
 }
 
-func templates(c *gin.Context) {
-	req := getParam(c, "id")
+func handleTemplates(c *gin.Context) {
+	if c.Request.Method == "GET" {
+		id := getParam(c, "id")
+		fmt.Println(len(id))
+		fmt.Println(id)
+	} else if c.Request.Method == "POST" {
+		webhookData := make(map[string]string)
+		err := json.NewDecoder(c.Request.Body).Decode(&webhookData)
+
+		if err != nil {
+			http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println(webhookData["data"])
+		templates.CreateTemplate("001", templates.Template{
+			Name:           "Test",
+			TemplateString: webhookData["data"],
+		})
+	} else {
+		http.Error(c.Writer, "Invalid payload", http.StatusBadRequest)
+	}
 }
 
 func getParam(c *gin.Context, paramName string) string {
