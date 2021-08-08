053e3d0c3684f375cd5d57ab743c212df14bf53b
Reme Le Hen <reme.lehane@gmail.com>
2021-08-08 07:13:30 +0200

    refactor(app): [] updated webhooks tow ork with gin Context

--- api/infrastructure/web-hooks/main.go
+++ api/infrastructure/web-hooks/main.go
@@ -6,19 +6,15 @@ import (
 
 	templates "github.com/RemeJuan/lattr/business/template"
 	"github.com/RemeJuan/lattr/infrastructure/twitter-client"
+	"github.com/gin-gonic/gin"
 )
 
-func HandleWebhook(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Method is not supported.", http.StatusBadRequest)
-		return
-	}
-
+func HandleWebhook(c *gin.Context) {
 	webhookData := make(map[string]string)
-	err := json.NewDecoder(r.Body).Decode(&webhookData)
+	err := json.NewDecoder(c.Request.Body).Decode(&webhookData)
 
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -27,7 +23,7 @@ func HandleWebhook(w http.ResponseWriter, r *http.Request) {
 	} else if len(webhookData["data"]) > 0 {
 		twitter_client.CreateTweet(webhookData["data"])
 	} else {
-		http.Error(w, "Invalid payload", http.StatusBadRequest)
+		http.Error(c.Writer, "Invalid payload", http.StatusBadRequest)
 	}
 }
 
