d7e1f26996efe3054d5f9ae8928200d0b079465c
Reme Le Hen <reme.lehane@gmail.com>
2021-08-10 09:31:47 +0200

    fix(database): [LAT-14] updated connection string config

--- api/infrastructure/postgress-db/postgress-db.go
+++ api/infrastructure/postgress-db/postgress-db.go
@@ -3,33 +3,14 @@ package postgress_db
 import (
 	"fmt"
 	"os"
-	"strconv"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 )
 
-type pgConnection struct {
-	host     string
-	port     int
-	user     string
-	password string
-	dbname   string
-}
-
 func Connect() *gorm.DB {
-	connection := connectionDetails()
-	host := connection.host
-	port := connection.port
-	user := connection.user
-	password := connection.password
-	dbname := connection.dbname
-
-	// connection string
-	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", host, port, user, password, dbname)
-
 	// open database
-	db, err := gorm.Open("postgres", psqlconn)
+	db, err := gorm.Open("postgres", os.Getenv("DATABASE_URL"))
 	CheckError(err)
 
 	// Close the Connection in the calling function
@@ -37,18 +18,6 @@ func Connect() *gorm.DB {
 	fmt.Println("Connected!")
 
 	return db
-}
-
-func connectionDetails() pgConnection {
-	port, _ := strconv.ParseInt(os.Getenv("PG_PORT"), 10, 32)
-
-	return pgConnection{
-		host:     os.Getenv("PG_HOST"),
-		port:     int(port),
-		user:     os.Getenv("PG_USER"),
-		password: os.Getenv("PG_PASSWORD"),
-		dbname:   os.Getenv("PG_DB_NAME"),
-	}
 }
 
 func CheckError(err error) {
