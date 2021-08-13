328db9dea2b3400f66c37410209df11605e306f2
Reme Le Hane <reme.lehane@gmail.com>
2021-08-13 19:51:35 +0200

    none(errors): [] updated reponse message

--- api/utils/error_formats/error_formats.go
+++ api/utils/error_formats/error_formats.go
@@ -14,7 +14,7 @@ func ParseError(err error) error_utils.MessageErr {
 		if strings.Contains(err.Error(), "no rows in result set") {
 			return error_utils.NotFoundError("no record matching given id")
 		}
-		return error_utils.InternalServerError(fmt.Sprintf("error when trying to save message: %s", err.Error()))
+		return error_utils.InternalServerError(fmt.Sprintf("error when trying to save data: %s", err.Error()))
 	}
 
 	return error_utils.InternalServerError(fmt.Sprintf("error when processing request: %s", err.Error()))
