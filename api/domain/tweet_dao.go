6b69714761e181b150e48cda2a487f33b2c46393
Reme Le Hane <reme.lehane@gmail.com>
2021-08-23 17:43:16 +0200

    fix(scheduler): [] updated query to get last scheduled post regardless of status

--- api/domain/tweet_dao.go
+++ api/domain/tweet_dao.go
@@ -21,7 +21,7 @@ var (
 	queryGetAllTweets          = "SELECT * FROM tweets WHERE UserId=$1;"
 	queryDeleteTweet           = "DELETE FROM tweets WHERE id=$1;"
 	queryGetPendingTweets      = "SELECT * FROM tweets WHERE Status != 'Posted' AND PostTime <= now() order by PostTime asc LIMIT 1"
-	queryGetLastScheduledTweet = "SELECT PostTime FROM tweets WHERE Status='Scheduled' ORDER by PostTime desc LIMIT 1"
+	queryGetLastScheduledTweet = "SELECT PostTime FROM tweets ORDER by PostTime desc LIMIT 1"
 )
 
 type TweetRepoInterface interface {
