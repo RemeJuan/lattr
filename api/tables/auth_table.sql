3704c8340049adc1514b3bc96c35ed74a578bf76
Reme Le Hane <reme.lehane@gmail.com>
2021-09-01 10:42:28 +0200

    fix(auth): [LAT-22] fixed table create

--- api/tables/auth_table.sql
+++ api/tables/auth_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE tweets
     Id       	SERIAL PRIMARY KEY,
     NAME			VARCHAR(300),
     Token			VARCHAR(300),
-    Scopes 		[]string
+    Scopes 		text[]
     ExpiresAt TIMESTAMP,
     CreatedAt TIMESTAMP,
     Modified  TIMESTAMP
