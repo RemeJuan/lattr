e20c19ec7712acb2a86df8d93080751c1d02c0b8
Reme Le Hane <reme.lehane@gmail.com>
2021-09-01 10:26:18 +0200

    feat(auth): [] added scopes and expiry date to table

--- api/tables/auth_table.sql
+++ api/tables/auth_table.sql
@@ -3,6 +3,8 @@ CREATE TABLE tweets
     Id       	SERIAL PRIMARY KEY,
     NAME			VARCHAR(300),
     Token			VARCHAR(300),
+    Scopes 		[]string
+    ExpiresAt TIMESTAMP,
     CreatedAt TIMESTAMP,
     Modified  TIMESTAMP
 );
