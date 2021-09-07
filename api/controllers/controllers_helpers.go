4f8f88e9eb69cb2adf28c0698bbe1ca7c0ed8961
Reme Le Hane <reme.lehane@gmail.com>
2021-09-07 13:58:32 +0200

    fix(auth): [LAT-22] removed helper

--- api/controllers/controllers_helpers.go
+++ api/controllers/controllers_helpers.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/RemeJuan/lattr/domain"
@@ -10,19 +9,6 @@ import (
 	"github.com/RemeJuan/lattr/utils/error_utils"
 	"github.com/gin-gonic/gin"
 )
-
-func GetIdFromParam(c *gin.Context) (*int64, error) {
-	paramId := GetParam(c, "id")
-	tkId, err := strconv.ParseInt(paramId, 10, 64)
-
-	if err != nil {
-		theErr := error_utils.UnprocessableEntityError("unable to parse ID")
-		c.JSON(theErr.Status(), theErr)
-		return nil, err
-	}
-
-	return &tkId, err
-}
 
 func GetParam(c *gin.Context, paramName string) string {
 	return c.Params.ByName(paramName)
