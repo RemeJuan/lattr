7a9a0fe93c4733d8e175b587e4fbec355c543e6f
Reme Le Hane <reme.lehane@gmail.com>
2021-09-08 10:12:41 +0200

    feat(auth): [LAT-22] added middleware to tweet routes

--- api/app/routes.go
+++ api/app/routes.go
@@ -11,12 +11,13 @@ func Router() {
 	r := gin.Default()
 	tw := r.Group("/tweets")
 	{
-		tw.POST("/create", controllers.CreateTweet)
-		tw.GET("/:id", controllers.GetTweet)
-		tw.GET("/all/:userId", controllers.GetTweets)
-		tw.PUT("/:id", controllers.UpdateTweet)
-		tw.DELETE("/:id", controllers.DeleteTweet)
+		tw.POST("/create", controllers.AuthenticateMiddleware("tweet:create"), controllers.CreateTweet)
+		tw.GET("/:id", controllers.AuthenticateMiddleware("tweet:read"), controllers.GetTweet)
+		tw.GET("/all/:userId", controllers.AuthenticateMiddleware("tweet:read"), controllers.GetTweets)
+		tw.PUT("/:id", controllers.AuthenticateMiddleware("tweet:update"), controllers.UpdateTweet)
+		tw.DELETE("/:id", controllers.AuthenticateMiddleware("tweet:delete"), controllers.DeleteTweet)
 	}
+	r.POST("/webhook", controllers.AuthenticateMiddleware("tweet:create"), controllers.WebHook)
 
 	tk := r.Group("/token")
 	{
@@ -27,6 +28,5 @@ func Router() {
 		tk.DELETE("/:id", controllers.AuthenticateMiddleware("token:delete"), controllers.DeleteToken)
 	}
 
-	r.POST("/webhook", controllers.WebHook)
 	log.Fatalln(r.Run())
 }
