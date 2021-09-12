9f3e95c555d4b2ba7dc5631876042ea640a0217a
Reme Le Hane <reme.lehane@gmail.com>
2021-09-12 07:36:10 +0200

    fix(swagger): [] moved top level swagger definitions to main

--- api/app/routes.go
+++ api/app/routes.go
@@ -11,28 +11,6 @@ import (
 	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
-// @title lattr API
-// @version 1.0
-// @description API driven Tweet scheduler written in Go
-
-// @contact.name Reme
-// @contact.url https://github.com/RemeJuan/lattr
-// @contact.email dev@lattr.app
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host https://api.lattr.app
-// @BasePath /api/
-
-// @securityDefinitions.basic BasicAuth
-
-// @securityDefinitions.apikey ApiKeyAuth
-// @in header
-// @name Authorization
-// @scope.tweet:create Grants write access
-// @scope.tweet:read Grants read and write access to administrative information
-
 func Router() {
 	r := gin.Default()
 	tw := r.Group("/tweets")
--- api/main.go
+++ api/main.go
@@ -9,6 +9,26 @@ import (
 	"github.com/RemeJuan/lattr/utils/scheduler"
 )
 
+// @title lattr API
+// @version 1.0
+// @description API driven Tweet scheduler written in Go
+
+// @contact.name Reme
+// @contact.url https://github.com/RemeJuan/lattr
+// @contact.email dev@lattr.app
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host https://api.lattr.app
+// @BasePath /
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+// @scope.tweet:create Grants write access
+// @scope.tweet:read Grants read and write access to administrative information
+
 func main() {
 	log.Println("server started")
 
